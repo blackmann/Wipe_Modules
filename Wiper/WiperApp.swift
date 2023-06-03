2f7ce5e3c0aa2c2e8fbddb18b1d6fda484421c94
Degreat <mail@degreat.co.uk>
2023-06-03 02:19:39 +0000

    Initial Commit

---
+++ Wiper/WiperApp.swift
@@ -0,0 +1,20 @@
+//
+//  WiperApp.swift
+//  Wiper
+//
+//  Created by De-Great Yartey on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WiperApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
